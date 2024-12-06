964f2147ad821529e32e5903346a5c500a1879de
hiboudu68 <maxluffy12@gmail.com>
2024-12-06 02:39:26 +0100

    pour Tom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import Maze from './Maze/Maze';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  const [started, setStarted] = useState(false);
+  
+  const [credit, setCredit] = useState(false);
+  const [cgu, setCgu] = useState(false);
+
+  const handleCredit = () => {
+    setCredit(true)
+  }
+
+  const handleCgu = () => {
+    setCgu(true)
+  }
+
+  const handleRevenir = () => {
+    setCredit(false)
+    setCgu(false)
+  }
 
   return (
     <div className="App-header">
+      {!credit && !cgu &&
+        <>
+          <button onClick={handleCredit}>
+            Credit
+          </button>
+          <button onClick={handleCgu}>
+            CGU
+          </button>
+        </>
+      }
 
-      <Maze></Maze>
+      {credit &&
+        <div>
+          <p>jdshfjsgfjkewdsg</p>
+          <button onClick={handleRevenir}>
+            Revenir
+          </button>
+        </div>
+      }
+      {cgu &&
+        <div>
+          <p>rien</p>
+          <button onClick={handleRevenir}>
+            Revenir
+          </button>
+        </div>
+      }
+      
+
     </div>
   );
 }
